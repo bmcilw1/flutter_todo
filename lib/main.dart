9545e248183ef69971f9bb22ebc28cc1bc541feb
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2019-05-16 18:42:17 -0700

    Initial work on TODO

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter TODO',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter TODO List'),
     );
   }
 }
@@ -45,6 +45,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool _isChecked = true;
+
+  void _checkBox(bool newValue) {
+    setState(() {
+      _isChecked = newValue;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -91,13 +98,18 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Checkbox(value: _isChecked, 
+              onChanged: _checkBox
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              'Clean up after Fluffy',
             ),
+            Visibility(visible: false,
+              child: Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            )
           ],
         ),
       ),
